a9fd67e5abba1020bc517a7729f2bc749b9c885c
Artem Kutsan <artemkutsan@gmail.com>
2026-02-06 11:34:33 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// src/App.jsx
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="container">React - Productivity</h1>
+
+        <section>
+          <div className="container"></div>
+        </section>
+
+        <section>
+          <div className="container"></div>
+        </section>
+
+        <section>
+          <div className="container"></div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
