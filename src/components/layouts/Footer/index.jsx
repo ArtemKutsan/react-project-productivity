f5c73bb7dcc82f2a92917fc6c8dda13bfa8e967c
Artem Kutsan <artemkutsan@gmail.com>
2026-02-06 12:22:40 +0100

    add: Добавлен компонент Footer

---
+++ src/components/layouts/Footer/index.jsx
@@ -0,0 +1,10 @@
+// src/components/layouts/Footer/index.jsx
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2026 Productivity Project</p>
+    </footer>
+  );
+}
