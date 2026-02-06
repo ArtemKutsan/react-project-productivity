e3582212f8a577651aa28ea1d414b542023604ff
Artem Kutsan <artemkutsan@gmail.com>
2026-02-06 17:58:58 +0100

    add: Добавлен компонент Logo

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,8 @@
+// src/components/Logo/index.jsx
+import styles from './Logo.module.css';
+
+function Logo() {
+  return <img src="/icons/logo.svg" alt="Productivity Logo" className={styles.logo} />;
+}
+
+export default Logo;
