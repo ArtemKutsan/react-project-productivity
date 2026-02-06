1055017e9ef68eb238edd17166e4eedab8d5a6e2
Artem Kutsan <artemkutsan@gmail.com>
2026-02-06 17:40:49 +0100

    update: Доработана базовая сетка шаблона страниц

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ import Footer from './components/layouts/Footer';
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <h1 className="container">React - Productivity</h1>
+      <div className="layout">
+        <Header />
+        <main>
+          <h1 className="container">React - Productivity</h1>
 
-        <section>
-          <div className="container"></div>
-        </section>
+          <section>
+            <div className="container"></div>
+          </section>
 
-        <section>
-          <div className="container"></div>
-        </section>
+          <section>
+            <div className="container"></div>
+          </section>
 
-        <section>
-          <div className="container"></div>
-        </section>
-      </main>
+          <section>
+            <div className="container"></div>
+          </section>
+        </main>
+      </div>
       <Footer />
     </>
   );
