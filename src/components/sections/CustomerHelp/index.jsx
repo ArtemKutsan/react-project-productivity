6941b992fc7530aba3cbcbe7c2a1900cf39dcde4
Artem Kutsan <artemkutsan@gmail.com>
2026-02-07 19:44:07 +0100

    add: Добален компонент CustomerHelp (предварительная версия)

---
+++ src/components/sections/CustomerHelp/index.jsx
@@ -0,0 +1,52 @@
+// src/components/CustomerHelp/index.jsx
+import { useState } from 'react';
+import styles from './CustomerHelp.module.css';
+import { Button } from '../../ui/Button';
+import { frequentlyAskedQuestions } from './questions';
+
+function CustomerHelp() {
+  const [openId, setOpenId] = useState(1);
+
+  const toggle = (id) => {
+    setOpenId((prev) => (prev === id ? null : id));
+  };
+
+  return (
+    <div className="container">
+      <div className="content">
+        <div className={styles.customerHelp}>
+          <div className={styles.header}>
+            <h2 className="title text-base uppercase">Customer help</h2>
+            <p className="title text-3xl">Frequently asked questions</p>
+          </div>
+
+          <div className={styles.faq}>
+            {frequentlyAskedQuestions.map((item) => {
+              const isOpen = openId === item.id;
+
+              return (
+                <div key={item.id} className={styles.item}>
+                  <button className={styles.question} onClick={() => toggle(item.id)}>
+                    {item.question}
+                  </button>
+
+                  {isOpen && item.answer && (
+                    <div className={styles.answer}>
+                      <p>{item.answer}</p>
+
+                      {item.hasButton && (
+                        <Button className={styles.docButton}>Go to documentation</Button>
+                      )}
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CustomerHelp;
