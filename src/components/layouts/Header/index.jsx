669715c3e56c02957c6a2aa3732cc0815adc905d
Artem Kutsan <artemkutsan@gmail.com>
2026-02-07 14:51:18 +0100

    update: Добавлена кнопка Get started. Обновлены стили

--- src/components/layouts/Header/index.jsx
+++ src/components/layouts/Header/index.jsx
@@ -1,17 +1,22 @@
 // src/components/layouts/Header/index.jsx
 import styles from './Header.module.css';
 import Navigation from '../../Navigation';
+import { Button } from '../../ui/Button';
 import Logo from '../../Logo';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.inner}`}>
-        <Logo />
-        <Navigation />
+        <div style={{ display: 'flex', gap: '4rem', alignItems: 'center' }}>
+          <Logo />
+          <Navigation />
+        </div>
 
-        {/* <Socials /> */}
-        {/* <Button /> */}
+        <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+          {/* <Socials /> */}
+          <Button variant="primarySoft">Get started</Button>
+        </div>
       </div>
     </header>
   );
