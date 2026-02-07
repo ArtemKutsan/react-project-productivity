efe90230ec946ba29d17db4774c2440de9a720b8
Artem Kutsan <artemkutsan@gmail.com>
2026-02-07 14:36:01 +0100

    update: Подключены комопненты Customers и AboutUs и обновлена разметка

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,27 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
+import Hero from './components/sections/Hero';
+import Customers from './components/sections/Customers';
+import AboutUs from './components/sections/AboutUs';
 
 function App() {
   return (
     <>
       <div className="layout">
         <Header />
-        <main>
-          <h1 className="container">React - Productivity</h1>
-
-          <section>
-            <div className="container"></div>
-          </section>
-
-          <section>
-            <div className="container"></div>
-          </section>
-
-          <section>
-            <div className="container"></div>
-          </section>
-        </main>
+        <section id="hero">
+          <Hero />
+        </section>
+        <section id="customers">
+          <Customers />
+        </section>
       </div>
+      <main>
+        <section id="about-us">
+          <AboutUs />
+        </section>
+      </main>
       <Footer />
     </>
   );
