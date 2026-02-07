d7ac0a44170f203ca1bb8bc3a0908b582c2da11e
Artem Kutsan <artemkutsan@gmail.com>
2026-02-07 14:30:47 +0100

    add: Добавлен компонент Button

---
+++ src/components/ui/Button/index.jsx
@@ -0,0 +1,28 @@
+// src/components/Button/index.jsx
+import styles from './Button.module.css';
+
+const VARIANTS = {
+  primary: styles.primary,
+  primarySoft: styles.primarySoft,
+  secondarySoft: styles.secondarySoft,
+  outline: styles.outline,
+};
+
+export function Button({
+  variant = 'primary',
+  className = '',
+  ...props
+}) {
+  return (
+    <button
+      {...props}
+      className={[
+        styles.button,
+        VARIANTS[variant],
+        className,
+      ].join(' ')}
+    />
+  );
+}
+
+
