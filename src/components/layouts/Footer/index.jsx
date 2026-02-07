d5e889da3670deb30481dd7d40276c605d777681
Artem Kutsan <artemkutsan@gmail.com>
2026-02-07 19:41:43 +0100

    update: Доработан Footer

--- src/components/layouts/Footer/index.jsx
+++ src/components/layouts/Footer/index.jsx
@@ -1,10 +1,27 @@
 // src/components/layouts/Footer/index.jsx
 import styles from './Footer.module.css';
+import { Button } from '../../ui/Button';
+import Logo from '../../Logo';
+import Navigation from '../../Navigation';
+import Socials from '../../Socials';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2026 Productivity Project</p>
+      <div className={`container ${styles.inner}`}>
+        <div style={{ display: 'flex', gap: '4rem', alignItems: 'center' }}>
+          <Logo />
+          <Navigation />
+        </div>
+
+        <div style={{ display: 'flex', gap: '2rem', alignItems: 'center' }}>
+          <Socials />
+          <Button variant="primarySoft">Get started</Button>
+        </div>
+      </div>
+      <div className="container">
+        <p style={{ paddingLeft: '5.5rem' }}>Copyright © 2026 by Artem Kutsan</p>
+      </div>
     </footer>
   );
 }
